6a21b4f496fe2a5e5b742aada4baad1a4905462f
MichalP <majk-p@users.noreply.github.com>
2014-05-03 14:12:42 +0200

    Update farm.js

--- farm.js
+++ farm.js
@@ -2,12 +2,16 @@
 // @name TribalWarsFarmingScript
 // @namespace https://github.com/majk-p/Tribal-Wars-scripts
 // @description add shortcuts
-// @include     http://*.plemiona.*/game.php?village=*&screen=place
+// @include http://*.plemiona.*/game.php?village=*&screen=place
 // @require http://github.com/andris9/jStorage/raw/master/jstorage.js
 // @version 1.2
 // @grant none
 // @author http://michalp.net
 // ==/UserScript==
+
+function ucFirst(string) {
+    return string.substring(0, 1).toUpperCase() + string.substring(1).toLowerCase();
+}
 
 function waitForQuickbar(){
     if(! $('#quickbar_inner').length){
@@ -19,23 +23,24 @@ function waitForQuickbar(){
 
 function main(){
     //$.jStorage.set('init',0);
-    $('#quickbar_inner') .find('tr:eq(1) ul') .append("<li id='farma'><a href='#'><span> <img alt='Farmienie' src='http://cdn.plemiona.pl/8.20/20029/graphic/command/attack.png'>Farmienie</span></a></li>");
+    $('#quickbar_inner') .find('tr:eq(1) ul') .append("<li id='farma'><a href='#'><span> <img alt='Farmienie' src='http://cdn.plemiona.pl/8.20/20029/graphic/command/attack.png'>Farmienie</span></a></li>");    
     if (!$.jStorage.get('init') || JSON.parse($.jStorage.get('cords')) .length == $.jStorage.get('iterator')) {
         // initialize cords
         console.log("init");
         var cord = [
-            '123|123',
-            '456|456'
+            '281|497',
+            '283|497',
+            '289|500',
+            '289|502',
+            '292|501',
+            '292|499'
         ];
         $.jStorage.set('cords', JSON.stringify(cord));
         // set defaults
-        $.jStorage.set('lightCount', 10);
-        $.jStorage.set('spyCount', 1);
         $.jStorage.set('farmingActive', 0);
         // finish initialization
         $.jStorage.set('init', 1);
         $.jStorage.set('iterator', 0);
-
     }
     $('li#farma') .click(function (e) {
         e.preventDefault();
@@ -44,9 +49,13 @@ function main(){
     });
     if($.jStorage.get('farmingActive')){
         $('li#farma').toggleClass("active", 1);
-        $('#unit_input_light').val($.jStorage.get('lightCount'));
-        $('#unit_input_spy').val($.jStorage.get('spyCount'));
-$('.target-input-field').val(JSON.parse($.jStorage.get('cords')) [$.jStorage.get('iterator')]);
+        
+        $("#units_form .unitsInput").each(function(){
+            var arr = $(this).attr("id").split("_");
+            $(this).val($.jStorage.get("farm"+ucFirst(arr[arr.length-1])));
+        });
+        
+        $('.target-input-field').val(JSON.parse($.jStorage.get('cords')) [$.jStorage.get('iterator')]);
         $.jStorage.set('iterator', parseInt($.jStorage.get('iterator')) + 1);
         //$.jStorage.set('cords', JSON.stringify(coord));
         //console.log(JSON.parse($.jStorage.get('cords')));
