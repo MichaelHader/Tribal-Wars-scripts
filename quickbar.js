b2447e2f7450e5c9f6c88d19a461d904c8dadc76
Michael Hader <michael.hader@outlook.com>
2020-04-13 20:48:33 +0200

    updated resources to use this repo

--- quickbar.js
+++ quickbar.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name        TribalWars-quickbar
 // @namespace   http://example.net
-// @include     *.plemiona.pl/game.php?*
+// @include     https://*/game.php*
 // @require     http://code.jquery.com/jquery-1.9.1.min.js
 // @require     http://cdn.jquerytools.org/1.2.7/full/jquery.tools.min.js
 // @require     http://github.com/andris9/jStorage/raw/master/jstorage.js
-// @resource    style https://raw.githubusercontent.com/majk-p/Tribal-Wars-scripts/master/static/style.css
-// @resource    qbar https://raw.githubusercontent.com/majk-p/Tribal-Wars-scripts/master/static/quickbar.html
-// @resource    settings https://raw.githubusercontent.com/majk-p/Tribal-Wars-scripts/master/static/settings.html
+// @resource    style https://raw.githubusercontent.com/MichaelHader//Tribal-Wars-scripts/master/static/style.css
+// @resource    qbar https://raw.githubusercontent.com/MichaelHader/Tribal-Wars-scripts/master/static/quickbar.html
+// @resource    settings https://raw.githubusercontent.com/MichaelHader//Tribal-Wars-scripts/master/static/settings.html
 // @version     1
 // @grant       GM_getResourceText
 // @grant       GM_addStyle
@@ -35,15 +35,15 @@ $(function () {
     GM_addStyle(GM_getResourceText('style'));
     $('.maincell') .prepend(GM_getResourceText('qbar') .split('VILID') .join(gup('village')));
     $("body").append(GM_getResourceText('settings'));
-    
+
 
     $("a[rel=#settings]").overlay({mask: '#000'});
-    
+
     //hiding tab content except first one
     $("#tabs div.tab_content").not(":first").hide();
     // adding Active class to first selected tab and show
-    $("#tabs ul li:first").addClass("active").show(); 
- 
+    $("#tabs ul li:first").addClass("active").show();
+
     // Click event on tab
     $("#tabs ul li").click(function() {
         // Removing class of Active tab
@@ -51,17 +51,17 @@ $(function () {
         // Adding Active class to Clicked tab
         $(this).addClass("active");
         // hiding all the tab contents
-        $("#tabs div.tab_content").hide();       
+        $("#tabs div.tab_content").hide();
         // showing the clicked tab's content using fading effect
         $($('a',this).attr("href")).fadeIn('slow');
- 
+
         return false;
     });
-    
+
     // tab selection
-    
+
     // tabs actions
-    
+
     $("#tabs-1 #target_attack").click(function(e){   // saving settings for farming script
         e.preventDefault();
         $("#tabs-1 .unitsInput").each(function(){
@@ -69,7 +69,7 @@ $(function () {
             $.jStorage.set("farm"+ucFirst(arr[arr.length-1]), $(this).val());
             console.log("farm"+ucFirst(arr[arr.length-1])+"  "+$(this).val());
         });
-        var pattern = /\d{3}\|\d{3}/; 
+        var pattern = /\d{3}\|\d{3}/;
         var cords = new Array();
         var data= $("#tabs-1 textarea").val().split(",");
         for(var i=0; i<data.length; i++){
@@ -92,6 +92,6 @@ $(function () {
         textareaData = textareaData + JSON.parse($.jStorage.get('cords'))[i] + ", ";
     }
     $("#tabs-1 textarea").val(textareaData);
-    //   eof tabs-1 
+    //   eof tabs-1
     console.log("done");
 });
